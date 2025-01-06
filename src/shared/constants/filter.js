923bd6b2236c7e1592386753461f4bd39d9797d4
HyeonJunSong <music9801@naver.com>
2025-01-06 14:39:49 +0900

    #1 config: 필터 상수 정의

---
+++ src/shared/constants/filter.js
@@ -0,0 +1,18 @@
+const ottPlatforms = ["왓챠", "웨이브", "티빙"];
+const ottPlatformsImageSrc = {
+  왓챠: "/images/ottPlatform/watcha.png",
+  웨이브: "/images/ottPlatform/wavve.png",
+  티빙: "/images/ottPlatform/tving.png",
+};
+
+const ottProfiles = {
+  왓챠: ["1번", "2번", "3번", "4번"],
+  웨이브: ["1번", "2번", "3번", "4번"],
+  티빙: ["1번", "2번", "3번", "4번"],
+};
+
+const sortOptions = ["최신순", "이름순", "플랫폼순"];
+
+const inclusionOptions = ["포함", "미포함"];
+
+export { ottPlatforms, ottPlatformsImageSrc, ottProfiles, sortOptions, inclusionOptions };
