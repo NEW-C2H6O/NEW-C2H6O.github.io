eafb414c6a085c2cfe8a780b6956b3fd6bc4f534
HyeonJunSong <music9801@naver.com>
2025-01-06 14:40:23 +0900

    #1 feat: 필터 바텀시트 UI

---
+++ src/pages/reservationHistory/componets/filterBottomSheet.jsx
@@ -0,0 +1,81 @@
+import { BottomSheet } from "widgets";
+import styles from "../style/filterBottomSheet.module.css";
+import { FilterSelector } from "./filterSelector";
+import {
+  inclusionOptions,
+  ottPlatforms,
+  ottPlatformsImageSrc,
+  ottProfiles,
+  sortOptions,
+} from "shared";
+import { useReservationHistoryFilterStore } from "features";
+
+function FilterBottomSheet() {
+  const {
+    isOpen,
+    selectedOttPlatforms,
+    selectedOttPlatform,
+    selectedOttProfiles,
+    selectedSortOptions,
+    selectedPreviousInclusion,
+    selectedMyInclusion,
+
+    closeFilter,
+    toggleSelectedOttPlatform,
+    toggleSelectedOttProfile,
+    toggleSelectedSortOption,
+    toggleSelectedPreviousInclusion,
+    toggleSelectedMyInclusion,
+  } = useReservationHistoryFilterStore();
+
+  return (
+    <BottomSheet
+      isOpen={isOpen}
+      closeBottomSheet={closeFilter}
+      Content={() => (
+        <div className={styles.container}>
+          <FilterSelector
+            title={`OTT 플랫폼`}
+            items={ottPlatforms}
+            selectedItems={selectedOttPlatforms}
+            toggleSelectedItem={toggleSelectedOttPlatform}
+            iconSrc={ottPlatformsImageSrc}
+          />
+          {selectedOttPlatform ? (
+            <FilterSelector
+              title={`프로필`}
+              items={selectedOttPlatform ? ottProfiles[selectedOttPlatform] : []}
+              selectedItems={selectedOttProfiles}
+              toggleSelectedItem={toggleSelectedOttProfile}
+              iconSrc={null}
+            />
+          ) : null}
+          <FilterSelector
+            title={`정렬`}
+            items={sortOptions}
+            selectedItems={selectedSortOptions}
+            toggleSelectedItem={toggleSelectedSortOption}
+            iconSrc={null}
+          />
+          <FilterSelector
+            title={`지난 예약`}
+            items={inclusionOptions}
+            selectedItems={selectedPreviousInclusion}
+            toggleSelectedItem={toggleSelectedPreviousInclusion}
+            iconSrc={null}
+          />
+          <FilterSelector
+            title={`내 예약`}
+            items={inclusionOptions}
+            selectedItems={selectedMyInclusion}
+            toggleSelectedItem={toggleSelectedMyInclusion}
+            iconSrc={null}
+          />
+          <div className={styles.submitButton} onClick={() => {}}>{`적용하기`}</div>
+        </div>
+      )}
+    />
+  );
+}
+
+export { FilterBottomSheet };
