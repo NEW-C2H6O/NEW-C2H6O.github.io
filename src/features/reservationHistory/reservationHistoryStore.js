8e1e9c6d5c11ffdb40b65a2b7e53cc126999e49c
HyeonJunSong <music9801@naver.com>
2025-01-09 15:30:07 +0900

    #1 feat: 장부 조회 필터 상태관리

--- src/features/reservationHistory/reservationHistoryStore.js
+++ src/features/reservationHistory/reservationHistoryStore.js
@@ -6,6 +6,14 @@ const defaultState = {
     month: new Date().getMonth() + 1,
     date: new Date().getDate(),
   },
+
+  filter: {
+    ottPlatforms: [0],
+    ottProfiles: [1],
+    sortOption: null,
+    isPreviousIncluded: false,
+    isMyReservationIncluded: true,
+  },
 };
 
 const useReservationHistoryStore = create((set, get) => ({
@@ -14,6 +22,24 @@ const useReservationHistoryStore = create((set, get) => ({
   //actions
   init: () => set({ ...defaultState }),
   setDate: ({ year, month, date }) => set({ date: { year, month, date } }),
+  setFilter: ({
+    ottPlatforms,
+    ottProfiles,
+    sortOption,
+    isPreviousIncluded,
+    isMyReservationIncluded,
+  }) => {
+    console.log(sortOption);
+    set({
+      filter: {
+        ottPlatforms,
+        ottProfiles,
+        sortOption,
+        isPreviousIncluded,
+        isMyReservationIncluded,
+      },
+    });
+  },
 }));
 
 export { useReservationHistoryStore };
