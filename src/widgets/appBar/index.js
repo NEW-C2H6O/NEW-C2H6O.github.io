3bf8b71702177b4f4f617e63749d7bddaac2b6a6
HyeonJunSong <music9801@naver.com>
2025-01-12 17:57:56 +0900

    #5 bugfix: 앱바 로고 임포트 주소 오류

--- src/widgets/appBar/index.js
+++ src/widgets/appBar/index.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import './style/index.css';
+import React from "react";
+import "./style/index.css";
 
 function Navbar({ pageName }) {
   return (
-    <div className='navbar'>
-      <div className='logo'>
-        <img src='green_logo.png' alt='Logo' className='logo-img' />
-        <span className='page-name'>{pageName}</span>
+    <div className="navbar">
+      <div className="logo">
+        <img src="/green_logo.png" alt="Logo" className="logo-img" />
+        <span className="page-name">{pageName}</span>
       </div>
     </div>
   );
