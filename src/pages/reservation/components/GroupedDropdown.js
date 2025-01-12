344de1393f759086525cc68c1e510dec04f398c6
HyeonJeong <wgwjh05169@gmail.com>
2025-01-12 09:50:25 +0900

    feat(reservation): 그룹화된 드롭다운 컴포넌트

---
+++ src/pages/reservation/components/GroupedDropdown.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import Select from 'react-select';
+
+const selectBoxStyle = {
+  container: (provided) => ({
+    ...provided,
+    width: '100%',
+    borderRadius: '8',
+  }),
+};
+
+const groupStyles = {
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+};
+
+const groupBadgeStyles = {
+  backgroundColor: '#EBECF0',
+  borderRadius: '2em',
+  color: '#172B4D',
+  display: 'inline-block',
+  fontSize: 12,
+  fontWeight: 'normal',
+  lineHeight: '1',
+  minWidth: 1,
+  padding: '0.16666666666667em 0.5em',
+  textAlign: 'center',
+};
+
+const formatGroupLabel = (data) => (
+  <div style={groupStyles}>
+    <span style={{ fontSize: 16, width: 100 }}> {data.label}</span>
+    <span style={groupBadgeStyles}>{data.options.length - 1}</span>
+  </div>
+);
+
+function GroupedDropdown({ groupedOptions, onSelectedOption }) {
+  return (
+    <Select
+      options={groupedOptions}
+      onChange={onSelectedOption}
+      placeholder='선택'
+      styles={selectBoxStyle}
+      formatGroupLabel={formatGroupLabel}
+    />
+  );
+}
+
+export { GroupedDropdown };
