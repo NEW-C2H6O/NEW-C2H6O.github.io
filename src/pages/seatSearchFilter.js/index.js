26dd7c414f8bd93816fdbcb13c8d16047aa11307
HyeonJeong <wgwjh05169@gmail.com>
2025-01-20 21:48:53 +0900

    #12 style: 주석 삭제제

--- src/pages/seatSearchFilter.js/index.js
+++ src/pages/seatSearchFilter.js/index.js
@@ -23,17 +23,14 @@ function formatProfileOptions(otts) {
 }
 
 function formatDate(date, time) {
-  // Date에서 날짜 정보 가져오기
   const year = date.getFullYear();
-  const month = date.getMonth(); // 0부터 시작 (1월 = 0)
+  const month = date.getMonth();
   const day = date.getDate();
 
-  // Moment에서 시간 정보 가져오기
   const hours = time.hours();
   const minutes = time.minutes();
   const seconds = time.seconds();
 
-  // 새로운 Date 객체 생성
   return new Date(year, month, day, hours, minutes, seconds);
 }
 
