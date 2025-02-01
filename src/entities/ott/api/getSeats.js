982466b44ddae4af85ea993b3248f933d188b8e6
HyeonJeong <wgwjh05169@gmail.com>
2025-02-01 19:43:45 +0900

    #18 feat: 여석 조회 API 호출

---
+++ src/entities/ott/api/getSeats.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+export const getSeats = async (ott, start, end) => {
+  try {
+    const url = `${process.env.REACT_APP_API_URL}/ott/available?start=${start}&end=${end}`;
+    if (ott !== null) {
+      url += `ott=${ott.ottId}_${ott.value}`;
+    }
+    const response = await axios.get(url);
+    return response.data;
+  } catch (error) {
+    throw new Error('Failed to fetch seats');
+  }
+};
