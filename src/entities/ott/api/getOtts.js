d7ee66184d33b2235b01aae1abf20300ea857d00
HyeonJeong <wgwjh05169@gmail.com>
2025-02-01 20:07:13 +0900

    #18 feat: OTT 정보 조회 API 호출

---
+++ src/entities/ott/api/getOtts.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+export const getOtts = async () => {
+  try {
+    const response = await axios.get(`${process.env.REACT_APP_API_URL}/ott`);
+
+    return response.data;
+  } catch (error) {
+    throw new Error('Failed to fetch seats');
+  }
+};
