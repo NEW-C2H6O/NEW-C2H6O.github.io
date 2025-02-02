016809739884ba25053faef21831eee1f8f1fd5a
HyeonJeong <wgwjh05169@gmail.com>
2025-02-02 18:01:10 +0900

    #18: fix: 여석 검색 API 호출 시 URL 생성 수정

--- src/entities/ott/api/getSeats.js
+++ src/entities/ott/api/getSeats.js
@@ -2,11 +2,17 @@ import axios from 'axios';
 
 export const getSeats = async (ott, start, end) => {
   try {
-    const url = `${process.env.REACT_APP_API_URL}/ott/available?start=${start}&end=${end}`;
+    let url = `${process.env.REACT_APP_API_URL}/ott/available?`;
+    
+    url += `start=${start.toISOString().slice(0, 19)}`;
+    url += `&end=${end.toISOString().slice(0, 19)}`;
+    
     if (ott !== null) {
-      url += `ott=${ott.ottId}_${ott.value}`;
+      url += `&ott=${ott.ott}_${ott.value}`;
     }
+
     const response = await axios.get(url, { withCredentials: true });
+
     return response.data;
   } catch (error) {
     throw new Error('Failed to fetch seats');
