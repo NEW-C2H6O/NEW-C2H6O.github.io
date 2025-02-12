cfaf621558b2c825737be758f84b027704783a7f
정현정 <wgwjh05169@gmail.com>
2025-02-12 21:20:05 +0900

    feat: 여석이 검색되지 않은 페이지

--- src/pages/seatSearch/index.js
+++ src/pages/seatSearch/index.js
@@ -1,11 +1,14 @@
 import './style/index.css';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { SeatItem } from './components/SeatItem';
 import { SearchButton } from './components/SearchButton';
 import { useNavigate } from 'react-router-dom';
 import { useSeatStore, useFilterStore } from 'features';
 
 function formatDate(date) {
+  if (date == null) {
+    return 'MM/DD';
+  }
   const pad = (num) => num.toString().padStart(2, '0');
   const month = pad(date.getMonth() + 1);
   const day = pad(date.getDate());
@@ -14,6 +17,10 @@ function formatDate(date) {
 }
 
 function formatTimePair(start, end) {
+  if (start == null || end == null) {
+    return 'HH:MM - HH:MM';
+  }
+
   const formatTime = (date) => {
     const hours = String(date.getHours()).padStart(2, '0');
     const minutes = String(date.getMinutes()).padStart(2, '0');
@@ -42,10 +49,17 @@ function formatOtt(otts) {
 
 function SeatSearchPage() {
   const { start, end, selectedOttInfo } = useFilterStore();
+  const [conditionExist, setConditionExist] = useState(false);
+  useEffect(() => {
+    if ((start != null) & (end != null)) {
+      setConditionExist(true);
+    }
+  }, []);
 
   const { seats, fetchSeats } = useSeatStore();
   useEffect(() => {
-    fetchSeats({ otts: selectedOttInfo, start: start, end: end });
+    if (conditionExist)
+      fetchSeats({ otts: selectedOttInfo, start: start, end: end });
   }, [selectedOttInfo, start, end]);
 
   const navigate = useNavigate();
@@ -62,9 +76,16 @@ function SeatSearchPage() {
       </div>
 
       <div className='search-list'>
-        {seats.map((seat) => {
-          return <SeatItem seat={seat} />;
-        })}
+        {conditionExist ? (
+          seats.map((seat) => {
+            return <SeatItem seat={seat} />;
+          })
+        ) : (
+          <div className='notFoundGuide'>
+            <img src='images/pictogram/notFoundImage.png' width='50'></img>
+            <label>아쉽게도 일치하는 여석이 없습니다.</label>
+          </div>
+        )}
       </div>
     </div>
   );
