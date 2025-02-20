849754ae5312f2394684832cdc230c24d111db11
정현정 <wgwjh05169@gmail.com>
2025-02-20 10:28:03 +0900

    refactor: 오류 페이지 CSS 수정

--- src/pages/error/index.js
+++ src/pages/error/index.js
@@ -1,4 +1,4 @@
-import './index.css';
+import './styles/index.css';
 import { useNavigate } from "react-router-dom";
 
 function ErrorPage() {
@@ -6,16 +6,16 @@ function ErrorPage() {
 
   return (
     <div className="background">
-      <div id="container">
-        <div id="label-container">
+      <div className="container">
+        <div className="label-container">
           <img src="images/pictogram/outlet.png" width="32%"></img>
-          <label id="bold-label">서버와의 통신이 원활하지 않습니다.</label>
+          <label className="bold-label">서버와의 통신이 원활하지 않습니다.</label>
           <div>
             <label>서비스 이용에 불편을 끼쳐드려 죄송합니다.</label>
             <label>잠시 후 다시 시도해주세요.</label>
           </div>
         </div>
-        <button id="back-button" onClick={() => navigate("/")}>
+        <button className="back-button" onClick={() => navigate("/")}>
           BACK HOME
         </button>
       </div>
