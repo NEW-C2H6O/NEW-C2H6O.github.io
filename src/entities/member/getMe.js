467a02b447c6d3eb8dd257baec5dbdf6e9dcc7f0
HyeonJunSong <music9801@naver.com>
2025-03-09 14:40:53 +0900

    #38 feat: 재인증 로직 수정 및 리다이렉션

--- src/entities/member/getMe.js
+++ src/entities/member/getMe.js
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axios from "axios";
 
 export const getMe = async () => {
   try {
@@ -8,12 +8,12 @@ export const getMe = async () => {
       withCredentials: true,
     });
 
-    return response.data;
+    return response.data.data;
   } catch (error) {
     if (error.response?.status === 401) {
-      return { data: undefined };
+      return null;
     }
 
-    throw new Error('Failed to fetch seats');
+    throw new Error("Failed to fetch seats");
   }
 };
