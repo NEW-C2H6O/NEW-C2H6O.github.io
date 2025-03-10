1f7d046a41233014bd7aef2ea33a913dbe83873b
정현정 <wgwjh05169@gmail.com>
2025-03-10 17:10:13 +0900

    #60: refactor: 홈페이지 디자인 수정

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -4,10 +4,12 @@ function HomePage() {
   return (
     <div className={'home-page'}>
       <div className={'home-top'}> </div>
-      <div className='label-container'>
+      <div className='content-container'>
         <img src='/green_logo.png' width={'100px'}></img>
-        <label>나만의 영화관 1열을</label>
-        <label>KOMO와 함께</label>
+        <div className='labels'>
+          <label>나만의 영화관 1열을</label>
+          <label>KOMO와 함께</label>
+        </div>
       </div>
       <div className='home-bottom'> </div>
     </div>
